16569edb837bcdf133b14bf39080085f4a2ef3db
Mirko Westermeier <mirko@westermeier.de>
2020-09-21 13:30:56 +0200

    Add vue material

--- frontend/load.js
+++ frontend/load.js
@@ -1,5 +1,10 @@
 import Vue from 'vue'
+import VueMaterial from 'vue-material'
+import 'vue-material/dist/vue-material.min.css'
+import 'vue-material/dist/theme/default.css'
 import V073 from './V073.vue'
+
+Vue.use(VueMaterial)
 
 window.addEventListener("load", function() {
     new Vue({
