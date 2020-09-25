a127868964bf570073c195eea35966a939804e9e
Mirko Westermeier <mirko@westermeier.de>
2020-09-25 20:25:06 +0200

    Add simple app structure to V073 component

--- frontend/V073.vue
+++ frontend/V073.vue
@@ -1,15 +1,24 @@
 <template>
-    <p>Hi, I'm {{ name }}!</p>
+    <div class="page-container">
+        <md-app md-mode="reveal">
+
+            <md-app-toolbar class="md-accent">
+                <span class="md-title">v073</span>
+            </md-app-toolbar>
+
+            <md-app-content>
+                <p>TODO</p>
+            </md-app-content>
+
+        </md-app>
+    </div>
 </template>
+
+<style scoped>
+.md-app { max-height: 100vh }
+</style>
 
 <script>
 export default {
-    data() { return {
-        name: "Voty McVoteface",
-    }; }
-};
+}
 </script>
-
-<style scoped>
-p { color: red }
-</style>
