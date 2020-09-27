c3e9f9d3048a667003f4ac7716f30ee3c2aaf3e5
Mirko Westermeier <mirko@westermeier.de>
2020-09-28 00:02:14 +0200

    Improve vue component organization

--- frontend/load.js
+++ frontend/load.js
@@ -7,9 +7,10 @@ import 'vue-material/dist/vue-material.min.css'
 import 'vue-material/dist/theme/default.css'
 Vue.use(VueMaterial)
 
-import V073     from './V073.vue'
-import Start    from './Start.vue'
-import About    from './About.vue'
+import V073 from './V073.vue'
+
+import Start    from './components/Start.vue'
+import About    from './components/About.vue'
 
 const router = new VueRouter({routes: [
     {path: '/',         component: Start},
