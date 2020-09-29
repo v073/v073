ba660400040b98325c97639d5f31069174da899b
Mirko Westermeier <mirko@westermeier.de>
2020-09-29 23:44:55 +0200

    Add simple link to VotingCreate

--- frontend/components/TokenForm.vue
+++ frontend/components/TokenForm.vue
@@ -1,4 +1,19 @@
 <template>
+    <md-card md-with-hover>
+
+        <md-card-header>
+            <div class="md-title">v073</div>
+        </md-card-header>
+
+        <md-card-content>
+            <!-- TODO -->
+        </md-card-content>
+
+        <md-card-actions>
+            <md-button to="/create">Create a new voting</md-button>
+        </md-card-actions>
+
+    </md-card>
 </template>
 
 <script>
